93dd8081b87d96f30b517abca0c865ac8e66a224
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2026-03-21 22:50:37 +0100

    Added Navbar

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,10 @@
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-page">
+      <h1>Profile Page</h1>
+      <p>Manage your profile and settings here!</p>
+    </div>
+  );
+}
---
+++ src/pages/Recipe.tsx
@@ -0,0 +1,11 @@
+import "./Recipe.css";
+
+
+export default function Recipe() {
+  return (
+    <div className="recipe-page">
+      <h1>Recipe Page</h1>
+      <p>Details of the recipe will be displayed here!</p>
+    </div>
+  );
+}
